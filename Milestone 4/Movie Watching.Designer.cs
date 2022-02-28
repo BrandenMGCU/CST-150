8ee7a7a7e8337121ca1aac254e2e16b8657cf457
Branden M <brandenmcneill21@gmail.com>
2022-02-27 19:22:11 -0700

    Milestone 4, CST150

---
+++ Milestone 4/Movie Watching.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Milestone_4
+{
+    partial class Movie_Watching
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Movie_Watching));
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Location = new System.Drawing.Point(12, 12);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(777, 329);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            this.videoView1.Click += new System.EventHandler(this.videoView1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(341, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(575, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 64);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Quit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "Movie 1",
+            "Movie 2",
+            "Movie 3",
+            "Movie 4"});
+            this.listBox1.Location = new System.Drawing.Point(153, 374);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(163, 64);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select a movie";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Movie_Watching
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.videoView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Movie_Watching";
+            this.Text = "Movie_Watching";
+            this.Load += new System.EventHandler(this.Movie_Watching_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private Button button2;
+        private Button button3;
+        private ListBox listBox1;
+        private Label label1;
+    }
+}
---
+++ Milestone 4/Movie Watching.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using LibVLCSharp.Shared;
+using System.IO;
+using System.Windows.Threading;
+
+/*
+ * Milestone 4 by Branden M.
+ * CST-150
+ * Grand Canyon University
+ */
+
+
+namespace Milestone_4
+{
+    public partial class Movie_Watching : Form
+    {
+        public LibVLC _libVLC;
+        // This will allow us to access the VLC functions
+
+        public MediaPlayer _mp;
+        // This will allow us to access the video player function
+
+        public Media media;
+        // This will allow the use of media inside of the video player application
+
+        public bool isPlaying = false;
+        // This will check to see if the video is playing
+
+        public Movie_Watching()
+        {
+            InitializeComponent();
+
+            Core.Initialize();
+            // This will allow the use of the VLC player becuase the windows form I'm using doesn't suppor windows media player
+
+            _libVLC = new LibVLC();
+            _mp = new MediaPlayer(_libVLC);
+
+        }
+
+        private void Movie_Watching_Load(object sender, EventArgs e)
+        {
+            // This will hold the elements to the video player
+        }
+
+        private void videoView1_Click(object sender, EventArgs e)
+        { 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // This will pause the video
+            _mp.Pause();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // This will quit the video
+            _mp.Stop();
+            Movie_Watching.ActiveForm.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // This will play movie number 1
+            if (listBox1.SelectedIndex == 0)
+            {
+                var movie1 = (@"C:\Users\brand\source\repos\Milestone 4\sample movies\movie 1.mp4");
+                // This will set the standard for the movie that will play
+
+                _mp.Play(new Media(_libVLC, movie1));
+                // This will utilize VLC to play movie 1
+
+                isPlaying = true;
+                // This will set the video to play
+            }
+            
+            // This will play movie number 2
+            else if (listBox1.SelectedIndex == 1)
+            {
+                var movie2 = (@"C:\Users\brand\source\repos\Milestone 4\sample movies\movie 2.mp4");
+                // This will set the standard for the movie that will play
+
+                _mp.Play(new Media(_libVLC, movie2));
+                // This will utilize VLC to play movie 1
+
+                isPlaying = true;
+                // This will set the video to play
+            }
+
+            // This will play movie number 3
+            else if (listBox1.SelectedIndex == 2)
+            {
+                var movie3 = (@"C:\Users\brand\source\repos\Milestone 4\sample movies\movie 3.mp4");
+                // This will set the standard for the movie that will play
+
+                _mp.Play(new Media(_libVLC, movie3));
+                // This will utilize VLC to play movie 3
+
+                isPlaying = true;
+                // This will set the video to play
+            }
+
+            // This will play movie number 4
+            else if (listBox1.SelectedIndex == 3)
+            {
+                var movie4 = (@"C:\Users\brand\source\repos\Milestone 4\sample movies\movie 4.mp4");
+                // This will set the standard for the movie that will play
+
+                _mp.Play(new Media(_libVLC, movie4));
+                // This will utilize VLC to play movie 1
+
+                isPlaying = true;
+                // This will set the video to play
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // This will tell the user to select the movie they would like to watch
+        }
+    }
+        }
+ 
+
